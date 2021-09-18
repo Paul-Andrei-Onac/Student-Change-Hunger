3cf7b06caf9fec89d73ebfa7e7d637c7224c01d9
D0GGZY <86016007+D0GGZY@users.noreply.github.com>
2021-09-18 10:42:31 +0300

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,217 @@
+<?php get_header();?>
+
+	<div class="container pt-5 pb-5">
+
+	<?php if (have_posts()) : while(have_posts()) : the_post();?>
+
+		<?php the_content();?>
+
+	<?php endwhile; endif;?>
+
+
+</div>
+
+
+
+
+<div>
+	<img class="logo" src="http://localhost/wordpress/wp-content/uploads/2021/09/logo.png">
+		<nav>
+			<ul>
+				<li><a href="Student Change Hunger.html" style="text-decoration: none;"><font color="#303030">Home</a></li>
+				<li><a href="Student Change Hunger.html" style="text-decoration: none;"><font color="#303030">About</a></li>
+				<li><a href="Student Change Hunger.html" style="text-decoration: none;"><font color="#303030">Winners</a></li>
+				<li><a href="Student Change Hunger.html" style="text-decoration: none;"><font color="#303030">FAQ</a></li>
+			</ul>
+		</nav>
+</div>
+<div class="gradclass">
+</div>
+	<div>
+		<h1 class="firsth1">Be the change in <br>new Jersey</h1>
+		<p class="firstp"> The Federation of New Jersey Food Banks Challenges all students, staff, teachers, and parents to think of those less fortunate throughout the school year by organizing a food drive at their school,, beginning <strong>Monday, September 13, 2021 and Friday, January 14, 2022.</strong></p>
+		<section>
+			<img class="biglogo" src="http://localhost/wordpress/wp-content/uploads/2021/09/biglogo.png">
+		</section>
+	</div>
+<article id="art">
+	<div class="row">
+		<div class="column">
+			<img class="cfb" src="http://localhost/wordpress/wp-content/uploads/2021/09/cfb.png">
+				<div class="butcol">
+			<button class="but">Register</button>
+			<button class="butinfo">More Information</button>
+		</div>
+	</div>
+</article>
+<article id="art">
+		<div class="column">
+			<img class="food" src="http://localhost/wordpress/wp-content/uploads/2021/09/food.png">
+				<div class="butcol2">
+			<button class="but2">Register</button>
+			<button class="butinfo2">More Information</button>
+		</div>
+	</div>
+</article>
+<article id="art">
+		<div class="column">
+			<img class="full" src="http://localhost/wordpress/wp-content/uploads/2021/09/fulfill.png">
+				<div class="butcol3">
+			<button class="but3">Register</button>
+			<button class="butinfo3">More Information</button>
+		</div>
+	</div>
+</article>
+<article id="art">
+		<div class="column">
+			<img class="nor" src="http://localhost/wordpress/wp-content/uploads/2021/09/nor2.png">
+				<div class="butcol4">
+			<button class="but4">Register</button>
+			<button class="butinfo4">More Information</button>
+		</div>
+	</div>
+</article>
+<article id="art">
+		<div class="column">
+			<img class="friends" src="http://localhost/wordpress/wp-content/uploads/2021/09/friends.png">
+				<div class="butcol5">
+			<button class="but5">Register</button>
+			<button class="butinfo5">More Information</button>
+		</div>
+	</div>
+</article>
+	<div class="backgr">
+		<img class="grad2" src="http://localhost/wordpress/wp-content/uploads/2021/09/gradient.png">
+		<div>
+			<h2 class="about"> About Students Change Hunger</h2>
+			<p class="aboutp">Students Change Hunger (SCH) is a friendly competition where schools and youth community groups host food and/or fund drives at their schools to help feed our hungry New Jersey neighbors. Schools compete to donate the most food or funds to fight back against hunger in our state between Monday, September 13, 2021 and Friday, January 14, 2022. <br><br>
+
+			During the 2020/2021 SCH campaign, more than 100 schools participated, donating more than 56,000 pounds of food and $45,000 to benefit their local food banks. In total, these donated resources supported 181,000 meals for neighbors in need. <br><br>
+
+			This program not only helps to meet the need in our communities, but also teaches students the power of advocacy and outreach. Students are the next generation of leaders, and when they come together to support their communities, great things are possible!</p>
+			<button class="learn">Learn More</button>
+<article>
+	<div class="backicon">
+		<img class="icon" src="http://localhost/wordpress/wp-content/uploads/2021/09/mc.png">
+		<h1 class="nr">181,000</h1>
+		<p class="desc">Meals For Neighbors In Need</p>
+	</div>
+</article>
+<article>
+	<div class="backicon2">
+		<img class="icon" src="http://localhost/wordpress/wp-content/uploads/2021/09/hand.png">
+		<h1 class="nr">545,000</h1>
+		<p class="desc">Donated To Local Food Banks</p>
+	</div>
+</article>
+<article>
+	<div class="backicon3">
+		<img class="icon3" src="http://localhost/wordpress/wp-content/uploads/2021/09/carrot.png">
+		<h1 class="nr">56,000</h1>
+		<p class="desc">Pounds Of Food Donated</p>
+	</div>
+</article>
+</div>
+<div class="pad">
+</div>
+<div class="backsch">
+	<img class="tropico" src="http://localhost/wordpress/wp-content/uploads/2021/09/trophy2.png">
+	<h2 class="troph1">2020/2021 Students Change Hunger State Champion</h2>
+	<p class="tropp">Congratulations to the 2020/2021 Students Change Hunger State Champion, <b>Madison High School Key Club</b>! Students collected 4,462 pounds of food, raised $7,095, providing more than 25,000 meals to neighbors in need. Madison High School Key Club used creative ideas to participate during the pandemic, from using food collection bins at local businesses, to partnering with the Madison Police Department to transport food donations to their local food pantry. Students utilized CFBNJ’s virtual fundraising platform to raise funds through social media and email campaigns.</p>
+	<img class="tropimg" src="http://localhost/wordpress/wp-content/uploads/2021/09/Sch.png">
+</div>
+<article>
+	<div class="backiconz">
+		<img class="icon" src="http://localhost/wordpress/wp-content/uploads/2021/09/mc.png">
+		<h1 class="nr">25,000+</h1>
+		<p class="desc">Meals For Neighbors In Need</p>
+	</div>
+</article>
+<article>
+	<div class="backiconz2">
+		<img class="icon" src="http://localhost/wordpress/wp-content/uploads/2021/09/hand.png">
+		<h1 class="nr">$7,095</h1>
+		<p class="desc">Donated To Local Food Banks</p>
+	</div>
+</article>
+<article>
+	<div class="backiconz3">
+		<img class="icon3" src="http://localhost/wordpress/wp-content/uploads/2021/09/carrot.png">
+		<h1 class="nr">4,462</h1>
+		<p class="desc">Pounds Of Food Donated</p>
+	</div>
+</article>
+</div>
+</div>
+<div class="pad2">
+</div>
+
+<div>
+	<h1 class="faq">FREQUENTLY ASKED QUESTIONS</h1>
+</div>
+
+<div>
+	<img class="faqimg" src="http://localhost/wordpress/wp-content/uploads/2021/09/faqimg.png">
+</div>
+<div class="faqdiv">
+<button type="button" class="collapsible">Who can participate?</button>
+<div class="content">
+  <p>Any school or youth community group in New Jersey may participate in the competition, public or private.</p>
+</div>
+<button type="button" class="collapsible">Does the donated food stay in my area?</button>
+<div class="content">
+</div>
+<button type="button" class="collapsible">What is a food bank and how does it differ from other feeding organizations?</button>
+<div class="content">
+</div>
+<button type="button" class="collapsible">What is the New Jersey Federation of Food Banks?</button>
+<div class="content">
+</div>
+<button type="button" class="collapsible">How do I know when and where to drop off the food?</button>
+<div class="content">
+</div>
+<button type="button" class="collapsible">How can I learn more about hunger in New Jersey and the U.S.?</button>
+<div class="content">
+</div>
+
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
+
+</div>
+<div class="pad3">
+</div>
+
+<div class="lastd">
+	<img class="lastimg" src="http://localhost/wordpress/wp-content/uploads/2021/09/image.jpg">
+	<h1 class="lasth1">Now it's easier to sign up for SCH updates</h1>
+		<p class="lastp">Be the first to know about new events, our ideas, and co-op offers.</p>
+			<button class="lastb">REGISTER NOW</button>
+</div>
+<div class="foot">
+	<img class="logo2" src="http://localhost/wordpress/wp-content/uploads/2021/09/logobl.png">
+	<p class="copy">Copyright © 2021 | StudentsChangeHunger.org</p>
+	<nav class="navv">
+		<ul class="navv">
+			<li><a href="Student Change Hunger.html" style="text-decoration: none;"><font color="white">Home</a></li>
+			<li><a href="Student Change Hunger.html" style="text-decoration: none;"><font color="white">About</a></li>
+			<li><a href="Student Change Hunger.html" style="text-decoration: none;"><font color="white">Winners</a></li>
+			<li><a href="Student Change Hunger.html" style="text-decoration: none;"><font color="white">FAQ</a></li>
+		</ul>
+	</nav>
+</div>
+
+<?php get_footer();?>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Students Change Hunger</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head();?>
+
+</head>
+<body <?php body_class();?>>
+
+
+
+
+<header>
+	<!--- <div class="container">
+	<?php wp_nav_menu (
+
+			array(
+
+					'theme_locatio' => 'top-menu',
+					'menu_class' => 'navigation'
+			)
+
+	);?>
+	</div> --->
+</header> 
